c474be8752c2f258721c182b772ce9676010c747
TaifQureshi <54901175+TaifQureshi@users.noreply.github.com>
2022-01-12 15:27:58 +0530

    code setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name = 'iot_agriculture',
+    version='0.1',
+    author='Taif Qureshi',
+    author_email='mohdtaifqureshi@gmail.com',
+    packages=["iot_agriculture"],
+)
