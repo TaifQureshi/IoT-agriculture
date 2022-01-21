4c26293a6109a3c6970f10a6db5f9738572be971
TaifQureshi <54901175+TaifQureshi@users.noreply.github.com>
2022-01-21 12:46:02 +0530

    temp and humidity

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ import time
 sensor = Adafruit_DHT.DHT11
 while True:
     print("taking temp")
-    humidity, temperature = Adafruit_DHT.read_retry(sensor, 26)
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, 4)
 
     print('Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity))
     time.sleep(1)
