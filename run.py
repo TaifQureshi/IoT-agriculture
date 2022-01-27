8addc89cbe63c88d514ecbf3f2247ef2d7afe354
TaifQureshi <54901175+TaifQureshi@users.noreply.github.com>
2022-01-27 16:47:40 +0530

    pid

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import sys
 import importlib
 from iot_agriculture import set_logger, base_setting
 import os
+from typing import Dict
 
 if __name__ == "__main__":
     program = "runners."
@@ -17,4 +18,12 @@ if __name__ == "__main__":
     config_location = os.getcwd() + '/config'
     config = base_setting(config_location, "base.yml")
     set_logger(f"{os.getcwd()}/{config.get('logger_path')}/{inputs[0]}", stdout=stdout)
-    runner = getattr(model, inputs[0])(inputs, config_location)
+    runner = getattr(model, inputs[0])
+    pidfile_args: Dict[str, str] = {
+        "pidname": inputs[0],
+        "piddir": "/tmp",
+        "register_term_signal_handler": "",
+    }
+    from pid import PidFile
+    with PidFile(**pidfile_args):
+        runner(inputs, config_location)
